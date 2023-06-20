38fe8b6884f194a084f13fd61138eee10d2cb263
Jisan <jis4nx@gmail.com>
2023-06-20 17:05:06 +0600

    Set jwt in cookie with httpOnly flags

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,24 @@
 from rest_framework.generics import CreateAPIView
 from .serializers import RegisterSerializer
 from .models import User
+from LuminiteV2.settings import SIMPLE_JWT
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+"""Custom TokeObtainPairView"""
+
+
+class CustomTokenPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        res = super().post(request, *args, **kwargs)
+        token = res.data['access']
+        res.set_cookie(
+            key="Token",
+            value=token,
+            httponly=True,
+            expires=SIMPLE_JWT['ACCESS_TOKEN_LIFETIME']
+        )
+        return res
 
 
 class RegisterUser(CreateAPIView):
