554943dd0f7dccf7ae00e38c50f3ee74573ef12a
Jisan <jis4nx@gmail.com>
2023-11-23 18:49:39 +0600

    Deploy to railway app

--- LuminiteV2/settings.py
+++ LuminiteV2/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = [".onrender.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 INSTALLED_APPS = [
