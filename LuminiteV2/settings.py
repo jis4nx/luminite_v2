2eb3fe2e1e19020200fcdb15e49c00ea49bf2d03
Jisan <jis4nx@gmail.com>
2023-11-24 19:30:45 +0600

    Update settings.py

--- LuminiteV2/settings.py
+++ LuminiteV2/settings.py
@@ -147,7 +147,7 @@ STATIC_URL = "/static/"
 #     os.path.join(BASE_DIR, "public/static"),
 # ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
